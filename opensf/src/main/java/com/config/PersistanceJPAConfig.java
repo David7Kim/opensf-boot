bfd24ae67561d09e75c7d860751df1487b7db0d9
David7Kim <ntw1kim@gmail.com>
2022-11-29 13:29:07 +0900

    commit

--- opensf/src/main/java/com/config/PersistanceJPAConfig.java
+++ opensf/src/main/java/com/config/PersistanceJPAConfig.java
@@ -1,4 +1,4 @@
-/*package com.config;
+package com.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -70,9 +70,8 @@ public class PersistanceJPAConfig {
 
         Properties additionalProperties() {
             Properties properties = new Properties();
-            properties.setProperty("hibernate.hbm2ddl.auto", "create");
+            properties.setProperty("hibernate.hbm2ddl.auto", "update");
             properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
             return properties;
         }
 }
-*/
